16447bb102cef6631c6ac23fb34f380aa464decc
truongmm <truongmm@gmail.com>
2015-11-02 19:28:59 -0800

    Add Share Functionality

--- app/src/main/java/com/codepath/gridimagesearch/activities/ImageDisplayActivity.java
+++ app/src/main/java/com/codepath/gridimagesearch/activities/ImageDisplayActivity.java
@@ -1,20 +1,35 @@
 package com.codepath.gridimagesearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.codepath.gridimagesearch.R;
 import com.codepath.gridimagesearch.models.ImageResult;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ImageDisplayActivity extends AppCompatActivity {
 
     private ImageView ivFullImage;
+    private ShareActionProvider miShareAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +38,17 @@ public class ImageDisplayActivity extends AppCompatActivity {
 
         ImageResult imageResult = (ImageResult) getIntent().getParcelableExtra("image");
         ivFullImage = (ImageView) findViewById(R.id.ivFullImage);
-        Picasso.with(ImageDisplayActivity.this).load(imageResult.fullUrl).into(ivFullImage);
+        Picasso.with(ImageDisplayActivity.this).load(imageResult.fullUrl).into(ivFullImage, new Callback() {
+            @Override
+            public void onSuccess() {
+                setupShareIntent();
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
 
         getSupportActionBar().setTitle(imageResult.displayTitle);
     }
@@ -32,6 +57,9 @@ public class ImageDisplayActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_image_display, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch reference to the share action provider
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
         return true;
     }
 
@@ -42,5 +70,40 @@ public class ImageDisplayActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupShareIntent() {
+        ImageView ivImageResult = (ImageView) findViewById(R.id.ivFullImage);
+        Uri bmpUri = getLocalBitmapUri(ivImageResult);
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        miShareAction.setShareIntent(shareIntent);
+    }
+
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 }
